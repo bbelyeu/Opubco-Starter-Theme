87a8b14d01adda5c24e58d1c096475fcba474608
Brad Belyeu <bradleylamar@gmail.com>
2015-09-03 03:42:46 +0000

    changed tabs to spaces in footer & index

--- footer.php
+++ footer.php
@@ -1,42 +1,42 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+            <!-- footer -->
+            <footer class="footer" role="contentinfo">
 
-				<!-- Address Schema / Copyright -->
-				<div itemscope itemtype="http://schema.org/LocalBusiness">	
-					<p>
-						<span itemprop="name"><?php opubco_copyright( '2014', $echo = true ); ?> <?php bloginfo('name'); ?></span>
-					
-						<?php if(of_get_option( 'street-address' )){ 
-							echo '<br /><span itemprop="streetAddress">' . esc_html( of_get_option( 'street-address' ) ) . '</span><br />';
-						} ?>  		
-						<?php if(of_get_option( 'city' )){ 
-							echo '<span itemprop="addressLocality">' . esc_html( of_get_option( 'city' ) ) . '</span>, ';
-						} ?> 							
-						<?php if(of_get_option( 'state' )){ 
-							echo '<span itemprop="addressRegion">' . esc_html( of_get_option( 'state' ) ) . '</span>';
-						} ?>
-						<?php if(of_get_option( 'zip' )){ 
-							echo '<span itemprop="postalcode">' . esc_html( of_get_option( 'zip' ) ) . '</span>';
-						} ?>
-					
-						<?php if(of_get_option( 'phone' )){ 
-							echo '<br /><span itemprop="telephone">' . esc_html( of_get_option( 'phone' ) ) . '</span>';
-						} ?>
-					</p>
-				</div>
-				<!-- /Address Schema / Copyright -->
+                <!-- Address Schema / Copyright -->
+                <div itemscope itemtype="http://schema.org/LocalBusiness">
+                    <p>
+                        <span itemprop="name"><?php opubco_copyright( '2014', $echo = true ); ?> <?php bloginfo('name'); ?></span>
 
-				<!-- Footer Menu -->
-				<?php //wp_nav_menu( array('theme_location' => 'footer-menu' )); ?>
-				<!-- Footer Menu -->
+                        <?php if(of_get_option( 'street-address' )){
+                            echo '<br /><span itemprop="streetAddress">' . esc_html( of_get_option( 'street-address' ) ) . '</span><br />';
+                        } ?>
+                        <?php if(of_get_option( 'city' )){
+                            echo '<span itemprop="addressLocality">' . esc_html( of_get_option( 'city' ) ) . '</span>, ';
+                        } ?>
+                        <?php if(of_get_option( 'state' )){
+                            echo '<span itemprop="addressRegion">' . esc_html( of_get_option( 'state' ) ) . '</span>';
+                        } ?>
+                        <?php if(of_get_option( 'zip' )){
+                            echo '<span itemprop="postalcode">' . esc_html( of_get_option( 'zip' ) ) . '</span>';
+                        } ?>
 
-			</footer>
-			<!-- /footer -->
+                        <?php if(of_get_option( 'phone' )){
+                            echo '<br /><span itemprop="telephone">' . esc_html( of_get_option( 'phone' ) ) . '</span>';
+                        } ?>
+                    </p>
+                </div>
+                <!-- /Address Schema / Copyright -->
 
-		<?php wp_footer(); ?>
-		<?php
-		/* Use Google Analytics Plugin for Google Analytics: https://wordpress.org/plugins/google-analytics-for-wordpress/
-		*/
-		?>
-	</body>
+                <!-- Footer Menu -->
+                <?php //wp_nav_menu( array('theme_location' => 'footer-menu' )); ?>
+                <!-- Footer Menu -->
+
+            </footer>
+            <!-- /footer -->
+
+        <?php wp_footer(); ?>
+        <?php
+        /* Use Google Analytics Plugin for Google Analytics: https://wordpress.org/plugins/google-analytics-for-wordpress/
+        */
+        ?>
+    </body>
 </html>
--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php get_header(); ?>
 
 
-	<main role="main">
-		<!-- section -->
-		<section>
-			<?php
-			if ( have_posts() ) :
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
+    <main role="main">
+        <!-- section -->
+        <section>
+            <?php
+            if ( have_posts() ) :
+                // Start the Loop.
+                while ( have_posts() ) : the_post();
 
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
+                    /*
+                     * Include the post format-specific template for the content. If you want to
+                     * use this in a child theme, then include a file called called content-___.php
+                     * (where ___ is the post format) and that will be used instead.
+                     */
+                    get_template_part( 'content', get_post_format() );
 
-				endwhile;
-				// Previous/next post navigation.
-				get_template_part( 'content', 'pagination' );
+                endwhile;
+                // Previous/next post navigation.
+                get_template_part( 'content', 'pagination' );
 
-			else :
-				// If no content, include the "No posts found" template.
-				get_template_part( 'content', 'none' );
+            else :
+                // If no content, include the "No posts found" template.
+                get_template_part( 'content', 'none' );
 
-			endif;
-			?>
+            endif;
+            ?>
 
-		</section>
-		<!-- /section -->
+        </section>
+        <!-- /section -->
 
-		<?php get_sidebar(); ?>
+        <?php get_sidebar(); ?>
 
-		<div class="clear"></div>
-	</main>
+        <div class="clear"></div>
+    </main>
 
 <?php get_footer(); ?>
